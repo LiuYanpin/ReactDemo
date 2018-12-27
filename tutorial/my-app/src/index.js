cf873ff3a803e021a4d0b7210f1da00ec487f912
yanpliu <yanpliu@thoughtworks.com>
2018-12-27 10:48:25 +0800

    [Yanping Liu]-First example

---
+++ tutorial/my-app/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <div>
+    <h1>Hello, {formatName(user)}</h1>
+    {getGreeting()}
+  </div>
+);
+
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
